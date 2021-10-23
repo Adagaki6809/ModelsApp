504d582c0cfd600b3d06bd10fdbd574a1ba14226
Adagaki6809 <include.ofstream.h@gmail.com>
2021-10-23 11:02:28 +0300

    first commit

---
+++ ViewModels/IndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ModelsApp.Models;
+using System.Collections.Generic;
+
+namespace ModelsApp.ViewModels
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Phone> Phones { get; set; }
+        public IEnumerable<CompanyModel> Companies { get; set; }
+    }
+}
